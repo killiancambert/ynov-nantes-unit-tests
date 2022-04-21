0a455188222182a17e81416c931f06560e3cc990
marth154 <martin.danvers@gmail.com>
2022-04-21 13:47:48 +0200

    Create test E2E

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -2,4 +2,17 @@ Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+
+    I.wait(1)
+
+    //Go on recherche 
+    I.amOnPage('https://www.ynov-nantes.com/recherche/');
+
+    // Complete the search input
+    I.fillField("s", "info")
+
+    I.wait(1)
+
+    // See if "Bachelor Informatique"
+    I.see("Bachelor Informatique")
 });
