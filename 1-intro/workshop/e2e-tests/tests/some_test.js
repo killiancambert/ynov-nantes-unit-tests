db6ac3ae422ba67fc817461ac6e5304b76b10f02
marth154 <martin.danvers@gmail.com>
2022-04-21 14:11:57 +0200

    Correction

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -4,9 +4,9 @@ Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
 
     I.wait(1)
-
-    //Go on recherche 
-    I.amOnPage('https://www.ynov-nantes.com/recherche/');
+     
+    // Click on icon
+    I.click(locate(".icon-search"))
 
     // Complete the search input
     I.fillField("s", "info")
