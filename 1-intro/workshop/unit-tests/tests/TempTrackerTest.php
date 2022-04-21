fc9784008ba2410bab0eb9623a8d5da8676ed70f
killiancambert <killian.cambert@ynov.com>
2022-04-21 17:25:43 +0200

    unit tests intro

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,62 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
+
+    
     public function test_something() {
         $this->assertEquals(42, 42);
+    }
+    
+    public function test_is_value_added()
+    {
+        $tempTracker = new TempTracker();
+        
+        for ($x = 0; $x < 7; $x++) {
+            $tempTracker->insert(rand(5, 35));
+        }
+
+        $this->assertNotEmpty($tempTracker->get_temps());
+    }
+
+    public function test_is_max()
+    {
+        $tempTracker = new TempTracker();
+        
+        for ($x = 0; $x < 7; $x++) {
+            $tempTracker->insert(rand(5, 35));
+        }
+
+        $max = max($tempTracker->get_temps());
+
+        $this->assertEquals($max, $tempTracker->get_max());
+    }
+
+    public function test_is_min()
+    {
+        $tempTracker = new TempTracker();
+        
+        for ($x = 0; $x < 7; $x++) {
+            $tempTracker->insert(rand(5, 35));
+        }
+
+        $min = min($tempTracker->get_temps());
+
+        $this->assertEquals($min, $tempTracker->get_min());
+    }
+
+    public function test_is_mean()
+    {
+        $tempTracker = new TempTracker();
+
+        $sum = 0;
+        for ($x = 0; $x < 7; $x++) {
+            $temp = rand(5, 35);
+            $tempTracker->insert($temp);
+            $sum += $temp;
+        }
+        $avg = ($sum / count($tempTracker->get_temps()));
+
+        $this->assertEquals($avg, $tempTracker->get_mean());
+
     }
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -10,4 +10,29 @@ class FlattenTest extends TestCase
     {
         $this->assertEquals(1337, 1337);
     }
+
+    public function test_assert_is_array()
+
+    {
+        $variable = [1, 2, [3, 4]];
+
+        $this->assertIsArray($variable,"assert variable is array or not");
+    }
+
+    public function test_assert_is_only_integers()
+    {
+        $variable = [1, 2, [3, 4]];
+
+        $flattened = flatten($variable);
+
+        $this->assertContainsOnly('integer', $flattened);
+    }
+
+    public function test_array_has_string()
+    {
+        $variable = flatten([1, 2, ['Test', 4]]);
+
+        $this->assertNotContainsOnly('integer', $variable);
+    }
+    
 }
