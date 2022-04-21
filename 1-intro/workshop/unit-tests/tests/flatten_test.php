be43670e6e8b8a2d2cc0bc5923e98f70ba33864c
killiancambert <killian.cambert@ynov.com>
2022-04-21 17:25:43 +0200

    unit tests intro

--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -8,6 +8,31 @@ class FlattenTest extends TestCase
 {
     public function test_something()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEquals(1337, 1337);
     }
+
+    public function test_assert_is_array()
+
+    {
+        $variable = [1, 2, [3, 4]];
+
+        $this->assertIsArray($variable,"assert variable is array or not");
+    }
+
+    public function test_assert_is_only_integers()
+    {
+        $variable = [1, 2, [3, 4]];
+
+        $flattened = flatten($variable);
+
+        $this->assertContainsOnly('integer', $flattened);
+    }
+
+    public function test_array_has_string()
+    {
+        $variable = flatten([1, 2, ['Test', 4]]);
+
+        $this->assertNotContainsOnly('integer', $variable);
+    }
+    
 }
